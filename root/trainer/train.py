d3e3c09af1a3992913bc9ba77435d8bf888669ae
Bob <negenentwintig@hotmail.com>
2018-05-25 11:28:57 +0200

    MOVING UP

--- root/trainer/train.py
+++ root/trainer/train.py
@@ -12,25 +12,47 @@ from sklearn.model_selection import train_test_split
 from keras.preprocessing import sequence, image
 from keras.applications.vgg19 import VGG19
 from keras.applications.vgg16 import VGG16
+from multiprocessing import Pool
 import sklearn
 import argparse
 import os
 import matplotlib.pyplot as plt
 from PIL import Image
 
-for _,_,im_names in os.walk('./res/train'):
-    for im_name in im_names:
-        with Image.open('./res/train/'+im_name) as im:
-            i += 1
-            print(i, end='\r')
-            im = modify(im, 0.05)
-            im = autocrop(im)
-            im = fixsize(im, 512)
+# for _,_,im_names in os.walk('./res/train'):
+#     for im_name in im_names:
+#         with Image.open('./res/train/'+im_name) as im:
+#             i += 1
+#             print(i, end='\r')
+#             im = modify(im, 0.05)
+#             im = autocrop(im)
+#             im = fixsize(im, 512)
 
 import json
 from tensorflow.python.lib.io import file_io
 
 LABELS = 228
+
+import numpy as np
+from keras.callbacks import Callback
+from sklearn.metrics import confusion_matrix, f1_score, precision_score, recall_score
+
+class Metrics(Callback):
+    def on_train_begin(self, logs={}):
+        self.val_f1s = []
+        self.val_recalls = []
+        self.val_precisions = []
+    def on_epoch_end(self, epoch, logs={}):
+        val_predict = (np.asarray(self.model.predict(self.model.validation_data[0]))).round()
+        val_targ = self.model.validation_data[1]
+        _val_f1 = f1_score(val_targ, val_predict, average='micro')
+        _val_recall = recall_score(val_targ, val_predict)
+        _val_precision = precision_score(val_targ, val_predict)
+        self.val_f1s.append(_val_f1)
+        self.val_recalls.append(_val_recall)
+        self.val_precisions.append(_val_precision)
+        print(" — val_f1: %f — val_precision: %f — val_recall %f" %(_val_f1, _val_precision, _val_recall))
+metrics = Metrics()
 
 def load_data(path):
     """
@@ -76,64 +98,64 @@ def create_model():
     return model
 
 
+dirname, filename = os.path.split(os.path.abspath(__file__))
+local_dir = os.path.join(dirname, '../data')
+if os.path.isdir(local_dir):
+    data_dir = local_dir
+else:
+    data_dir = 'gs://fashiondataset'
+
+with open(data_dir + '/train.json') as json_data:
+    labels = {d['imageId']:d['labelId'] for d in json.load(json_data)['annotations']}
+
+def label(fn):
+    id = fn.split('/')[-1].split('.')[0]
+    # LOOK! WE SUBTRACT ONE! ALL LABELS ARE OFF BY ONE! e.g., 227 is actually 228.
+    labels = np.array([int(l) for l in labels]) - 1
+    one_hot_labels = np.zeros((LABELS), dtype=bool)
+    one_hot_labels[labels] = 1
+    return one_hot_labels
+
+def read_image(arg):
+    desired_size, subdir, im_name = arg
+    with Image.open(subdir+'/'+im_name) as im:
+        old_size = im.size
+        ratio = float(desired_size)/max(old_size)
+        new_size = tuple([int(x*ratio) for x in old_size])
+        im = im.resize(new_size, Image.ANTIALIAS)
+        new_im = Image.new("RGB", (desired_size, desired_size))
+        new_im.paste(im, ((desired_size-new_size[0])//2,
+        (desired_size-new_size[1])//2))
+
+        return new_im, label(im_name)
+
 def main(train_folder, test_file, job_dir):
     model = create_model()
     # model.fit(X_train, y_train, nb_epoch=1, batch_size=32, verbose=2)
     gen = image.ImageDataGenerator()
 
-    if os.path.isdir('../data'):
-        data_dir = '../data'
-    else:
-        data_dir = 'gs://imaterialist_challenge_data'
-
     def generator(subdir, batch_size):
-        file_names = np.array(os.listdir(subdir))
         desired_size = 256
+        file_names = [(desired_size, subdir, fn) for fn in os.listdir(subdir)]
         i = 0
         batch = np.zeros((batch_size, desired_size, desired_size, 3))
         labels = np.zeros((batch_size, LABELS))
+        p = Pool()
 
         while True:
-            for im_name in file_names:
-                with Image.open(subdir+'/'+im_name) as im:
-                    old_size = im.size
-                    ratio = float(desired_size)/max(old_size)
-                    new_size = tuple([int(x*ratio) for x in old_size])
-                    im = im.resize(new_size, Image.ANTIALIAS)
-                    new_im = Image.new("RGB", (desired_size, desired_size))
-                    new_im.paste(im, ((desired_size-new_size[0])//2,
-                    (desired_size-new_size[1])//2))
-
-                    batch[i] = new_im
-                    labels[i] = label(im_name)
-                    if i == batch_size-1:
-                        yield batch
-                        i = 0
-                    else:
-                        i += 1
-                    # im = modify(im, 0.05)
-                    # im = autocrop(im)
-                    # im = fixsize(im, 512)
+            for im, label in p.imap_unordered(read_image, file_names):
+                batch[i], labels[i] = im, label
+                if i == batch_size-1:
                     yield batch, labels
+                    i = 0
+                else:
+                    i += 1
 
     # labels = {d["imageId"]: d["labelId"] for d in json.loads('train.json')["annotations"]}
-
-    def label(fn):
-        labels = fn.split('[')[1].split(']')[0].split(', ')
-        # LOOK! WE SUBTRACT ONE! ALL LABELS ARE OFF BY ONE! e.g., 227 is actually 228.
-        labels = np.array([int(l) for l in labels]) - 1
-        one_hot_labels = np.zeros((LABELS), dtype=bool)
-        one_hot_labels[labels] = 1
-        return one_hot_labels
 
     # def generate(batch_size):
     #     while True:
     #         for name in os.walk(data_dir + '/train'):
-
-
-    def labels(gen):
-        idx = gen.batch_index * gen.batch_size
-        return np.array([label(fn) for fn in gen.filenames[idx : idx + gen.batch_size]])
 
     # gen = ((x, labels(data)) for (x, _) in data)
 
