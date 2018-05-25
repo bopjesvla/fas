fdccf9d069641093486928dc5cb53712d8cd1a6b
Matthijs Biondina <m.biondina@outlook.com>
2018-05-25 11:13:38 +0200

    rename_updated

--- rename.py
+++ rename.py
@@ -2,16 +2,21 @@ import os
 import json
 
 DATA_folder = './root/DATASET'
-image_folder = './res/train'
+image_folder = './root/data/train'
 
-with open(DATA_folder + '/train.json') as json_data:
-    labels = {d['imageId']:d['labelId'] for d in json.load(json_data)['annotations']}
+#with open(DATA_folder + '/train.json') as json_data:
+#    labels = {d['imageId']:d['labelId'] for d in json.load(json_data)['annotations']}
 
-for fn in os.listdir(image_folder):
-    idx = fn.split('.')[0]
-    string = 'id_' + str(idx) + '_labels_['
-    for l in labels[idx]:
-        string += l + ', '
-    string = string[:-2] + '].jpg'
-    os.rename(image_folder + fn, image_folder + string)   
+if False:
+    for fn in os.listdir(image_folder):
+        idx = fn.split('.')[0]
+        string = 'id_' + str(idx) + '_labels_['
+        for l in labels[idx]:
+            string += l + ', '
+        string = string[:-2] + '].jpg'
+        os.rename(image_folder + fn, image_folder + string)   
     
+if True:
+    for fn in os.listdir(image_folder):
+        idx = fn.split('_')[1]
+        os.rename(image_folder + '/' + fn, image_folder + '/' + idx + '.jpg')
