5cc69d7012bb7efca013a9bf220256929c6d8435
Bob <negenentwintig@hotmail.com>
2018-05-30 12:12:53 +0200

    init, predicting

--- predict.sh
+++ predict.sh
@@ -1,3 +1,6 @@
 . ~/.bashrc
-gsutil cp gs://fashiondataset/model.h5 data/model.h5
-python root/trainer/local_predict.py
+# gsutil cp gs://fashiondataset/model.h5 data/model.h5
+gcloud ml-engine jobs submit training kidzbop$RAND --stream-logs \
+       --runtime-version 1.4 --job-dir gs://fashiondataset/kidzbop$RAND \
+       --package-path root/trainer --module-name trainer.predict \
+       --region europe-west1 --config root/trainer/config.yaml \
