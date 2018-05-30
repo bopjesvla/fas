4d4c031c2839f282053c6365661f1f79ada78818
Bob <negenentwintig@hotmail.com>
2018-05-30 12:14:32 +0200

    rand

--- predict.sh
+++ predict.sh
@@ -1,5 +1,6 @@
 . ~/.bashrc
 # gsutil cp gs://fashiondataset/model.h5 data/model.h5
+RAND=$RANDOM$RANDOM
 gcloud ml-engine jobs submit training kidzbop$RAND --stream-logs \
        --runtime-version 1.4 --job-dir gs://fashiondataset/kidzbop$RAND \
        --package-path root/trainer --module-name trainer.predict \
