b710d0944f06d1b8f87b56625de4547f78efffd9
Bob <negenentwintig@hotmail.com>
2018-05-30 15:17:17 +0200

    no-batch predictions

--- root/trainer/predict.py
+++ root/trainer/predict.py
@@ -11,8 +11,8 @@ files = file_io.list_directory(test_dir)
 file_io.copy('gs://fashiondataset/model.h5', 'model.h5')
 
 model = keras.models.load_model('model.h5')
-gen = np.array([np.asarray(read_image((256, test_dir, fn), train=False)) for fn in files])
-scores = model.predict(gen, steps=len(files))
+gen = (np.array([np.asarray(read_image((256, test_dir, fn), train=False))]) for fn in files)
+scores = model.predict_generator(gen, steps=len(files), verbose=1)
 scores = scores > .5
 all_labels = np.arange(1, LABELS+1)
 scores = [all_labels[s] for s in scores]
